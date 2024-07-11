e365d6346960e9376177caf0f56353a2e2fed123
matinrnz <mrnz2717@gmail.com>
2024-07-11 20:49:03 +0330

    list comprehension

--- 14-us-states-game/main.py
+++ 14-us-states-game/main.py
@@ -34,13 +34,10 @@ while len(guessed_states) < 50:
     answer = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
                                     prompt="What's another state's name?").title()
     if answer == "Exit":
-        missing_states = []
-        for state in states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in states if state not in guessed_states]
         # states to learn
-        data = pandas.DataFrame(missing_states)
-        data.to_csv("learn.csv")
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("learn.csv")
         break
     if answer not in guessed_states:
         if check_answer():
