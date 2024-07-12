6bdb6999db7a9b039331be2b1aaa9f7e8245e067
matinrnz <mrnz2717@gmail.com>
2024-07-12 23:46:29 +0330

    update

--- 15-NATO-alphabet/main.py
+++ 15-NATO-alphabet/main.py
@@ -1,11 +1,10 @@
 import pandas
 
 phonetic_alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
-# TODO 1. Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
+# Create a dictionary in this format: {"A": "Alfa", "B": "Bravo"}
 alphabet_dict = {row['letter']: row['code'] for (index, row) in phonetic_alphabet.iterrows()}
 # print(alphabet_dict)
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+# Create a list of the phonetic code words from a word that the user inputs.
 user_word = input("Enter a word: ").upper()
 result_dict = [alphabet_dict[letter] for letter in user_word]
 print(result_dict)
