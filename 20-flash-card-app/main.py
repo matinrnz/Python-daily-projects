866f3b1fb8ebc00a4ebc859696bbb6af6f58f6b9
matinrnz <mrnz2717@gmail.com>
2024-07-21 22:32:18 +0330

    [Add] flip card and save progress

--- 20-flash-card-app/main.py
+++ 20-flash-card-app/main.py
@@ -2,14 +2,41 @@ from tkinter import *
 import pandas as pd
 import random
 # ---------------------------- WORD GENERATOR ------------------------------- #
-data = pd.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient="records")
+current_card = {}
+to_learn = {}
+
+try:
+    data = pd.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pd.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
 
 def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=current_card["French"])
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=front_img)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+# ---------------------------- FLIP CARD ------------------------------- #
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=back_img)
+
+
+# ---------------------------- SAVE PROGRESS ------------------------------- #
+def is_known():
+    to_learn.remove(current_card)
+    data_to_learn = pd.DataFrame(to_learn)
+    data_to_learn.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -17,18 +44,21 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 window = Tk()
 window.title("Flashy")
-window.config(padx=50, pady=50,bg=BACKGROUND_COLOR)
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+flip_timer = window.after(3000, func=flip_card)
 
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 front_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=front_img)
+back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=front_img)
 card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold"))
 canvas.grid(row=0, column=0, columnspan=2)
 
 # Buttons
 right_image = PhotoImage(file="images/right.png")
-check_button = Button(image=right_image, highlightthickness=0, command=next_card)
+check_button = Button(image=right_image, highlightthickness=0, command=is_known)
 check_button.grid(row=1, column=1)
 
 wrong_image = PhotoImage(file="images/wrong.png")
