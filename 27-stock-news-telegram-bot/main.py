07247c535d6b522466d5c59e5843b42d46da79f4
matinrnz <mrnz2717@gmail.com>
2024-08-03 11:35:37 +0330

    [Add] telegram bot

--- 27-stock-news-telegram-bot/main.py
+++ 27-stock-news-telegram-bot/main.py
@@ -1,43 +1,55 @@
 import requests
 import os
+from telegram import Update
+from telegram.ext import *
 
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
-STOCK_KEY = os.environ.get("STOCK_KEY")
-NEWS_KEY = os.environ.get("NEWS_KEY")
+
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
+STOCK_KEY = os.environ.get("STOCK_KEY")
+NEWS_KEY = os.environ.get("NEWS_KEY")
+TOKEN = os.environ.get("TOKEN")
+BOT_USERNAME = os.environ.get("BOT_USERNAME")
+
+
 # STEP 1: Use https://www.alphavantage.co/documentation/#daily
-# When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+# When stock price increase/decreases by 5% between yesterday and the day before yesterday then send the articles.
 stock_params = {
     "function": "TIME_SERIES_DAILY",
     "symbol": STOCK_NAME,
     "apikey": STOCK_KEY,
 }
 response = requests.get(STOCK_ENDPOINT, params=stock_params)
-response.raise_for_status()
-data = response.json()
-data_list = [value for (key,value) in data.items()]
-# print(data_list)
+# print(response.json())
+data = response.json()["Time Series (Daily)"]
+data_list = [value for (key, value) in data.items()]
+
 # 1- Get yesterday's closing stock price.
 yesterday_data = data_list[0]
 yesterday_price = float(yesterday_data["4. close"])
 # 2- Get the day before yesterday's closing stock price
 day_before_yesterday_data = data_list[1]
-day_before_yesterday_price = float(yesterday_data["4. close"])
+day_before_yesterday_price = float(day_before_yesterday_data["4. close"])
+
 # 3- Find the positive difference between 1 and 2.
-difference = abs(yesterday_price - day_before_yesterday_price)
-# print(difference)
+difference = yesterday_price - day_before_yesterday_price
+up_down = None
+if difference > 0:
+    up_down = "🔺"
+else:
+    up_down = "🔻"
+
 # 4- Work out the percentage difference in price between closing price yesterday and closing price the day
 # before yesterday.
-diff_percentage = (difference / day_before_yesterday_price) * 100
-# 5- If percentage is greater than 5 then print("Get News").
-if diff_percentage > 5:
+diff_percentage = round((difference / day_before_yesterday_price) * 100)
+# 5- If percentage is greater than 5 then send the articles.
+if abs(diff_percentage) > 5:
     # print("Get News")
     # STEP 2: https://newsapi.org/
-    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
-    # 6- Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
+    # 6- Use the News API to get articles related to the COMPANY_NAME.
     news_params = {
         "q": COMPANY_NAME,
         "apiKey": NEWS_KEY,
@@ -51,19 +63,36 @@ if diff_percentage > 5:
 
     # STEP 3: Use telegram bot api
     # to send a separate message with each article's title and description.
+    # 8- Create a new list of the first 3 articles' headline and description.
+    formatted_articles = [(f"{STOCK_NAME}: {up_down}{diff_percentage}%\nHeadline: {article['title']}. \n"
+                           f"Brief: {article['description']}") for article in three_articles]
 
-# 8- Create a new list of the first 3 article's headline and description using list comprehension.
+    # formatted_articles = ["article1", "article2", "article3"]
+    # 9- Send each article as a separate message via Telegram.
+    async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+        await update.message.reply_text("Hello! I send you 3 daily stock articles.")
 
-# 9- Send each article as a separate message via Telegram.
 
-# Optional: Format the message like this:
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
+    async def news_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+        for article in formatted_articles:
+            await update.message.reply_text(article)
 
+
+    async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+        print(f"Update: {update} caused error {context.error}")
+
+
+    if __name__ == '__main__':
+        print('Starting bot...')
+        app = Application.builder().token(TOKEN).build()
+
+        # Commands
+        app.add_handler(CommandHandler('start', start_command))
+        app.add_handler(CommandHandler('news', news_command))
+
+        # Errors
+        app.add_error_handler(error)
+
+        # Polls the bot
+        print('Polling...')
+        app.run_polling(poll_interval=3)
