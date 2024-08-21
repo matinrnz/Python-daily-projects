c3f03435d170790e9fa4364532899ef632f4c17e
matinrnz <mrnz2717@gmail.com>
2024-08-21 21:45:43 +0330

    [Add] find_elements

--- 38-selenium/main.py
+++ 38-selenium/main.py
@@ -19,8 +19,19 @@ driver.get("https://www.python.org")
 # print(button.size)
 # documentation_link = driver.find_element(By.CSS_SELECTOR, value=".documentation-widget a")
 # print(documentation_link.text)
-bug_link = driver.find_element(By.XPATH, value='//*[@id="site-map"]/div[2]/div/ul/li[3]/a')
-print(bug_link.text)
+# bug_link = driver.find_element(By.XPATH, value='//*[@id="site-map"]/div[2]/div/ul/li[3]/a')
+# print(bug_link.text)
+
+event_times = driver.find_elements(By.CSS_SELECTOR, value=".event-widget.last time")
+event_names = driver.find_elements(By.CSS_SELECTOR, value=".event-widget.last li a")
+events = {}
+
+for n in range(len(event_times)):
+    events[n] = {
+        "time": event_times[n].text,
+        "name": event_names[n].text,
+    }
+print(events)
 
 # driver.close()
 driver.quit()
